b517594486bdb8b0e58a4122f819d85e0f96d73a
KhanAnupamShafi <deep71.bd@gmail.com>
2024-05-05 02:04:53 +0600

    mongo uri updated 3

--- services/dbconnect.js
+++ services/dbconnect.js
@@ -1,23 +1,59 @@
+// import mongoose from "mongoose";
+
+// let cachedDb = null;
+
+// export async function dbConnect() {
+//   if (cachedDb) {
+//     console.log("Using existing database connection");
+//     return cachedDb;
+//   }
+
+//   try {
+//     const conn = await mongoose.connect(
+//       "mongodb+srv://vercel-admin-user-66368bfaab78d05a20034636:5aPBzHYAWqgRGJBM@aws-ap-south-1.hcihgob.mongodb.net/khanaKhazana"
+//     );
+
+//     console.log("Connected to MongoDB");
+//     cachedDb = conn;
+//     return cachedDb;
+//   } catch (err) {
+//     console.error("Error connecting to MongoDB:", err);
+//     throw new Error("Could not connect to MongoDB");
+//   }
+// }
 import mongoose from "mongoose";
 
-let cachedDb = null;
+const DATABASE_URL =
+  "mongodb+srv://vercel-admin-user-66368bfaab78d05a20034636:5aPBzHYAWqgRGJBM@aws-ap-south-1.hcihgob.mongodb.net/khanaKhazana?retryWrites=true&w=majority";
 
-export async function dbConnect() {
-  if (cachedDb) {
-    console.log("Using existing database connection");
-    return cachedDb;
-  }
-
-  try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://vercel-admin-user-66368bfaab78d05a20034636:5aPBzHYAWqgRGJBM@aws-ap-south-1.hcihgob.mongodb.net/khanaKhazana"
-    );
-
-    console.log("Connected to MongoDB");
-    cachedDb = conn;
-    return cachedDb;
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err);
-    throw new Error("Could not connect to MongoDB");
-  }
+if (!DATABASE_URL) {
+  throw new Error(
+    "Please define the DATABASE_URL environment variable inside .env.local"
+  );
 }
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(DATABASE_URL, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
