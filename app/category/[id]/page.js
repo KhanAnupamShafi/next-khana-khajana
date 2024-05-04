20a3974dcdb92c41367a12961aea3205109c9869
KhanAnupamShafi <deep71.bd@gmail.com>
2024-05-05 00:06:59 +0600

    deleted middleware

--- app/category/[id]/page.js
+++ app/category/[id]/page.js
@@ -14,12 +14,12 @@ const page = async ({ params: { id } }) => {
   return (
     <section className='container py-8'>
       <div>
-        {recipes.length > 0 ? (
+        {recipes?.length > 0 ? (
           <>
             <h3 className='font-semibold text-xl'>{categoryName}</h3>
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-8 justify-items-center'>
               {recipes.map((recipe) => (
-                <RecipeCard key={recipe.id} recipeData={recipe} />
+                <RecipeCard key={recipe?.id} recipeData={recipe} />
               ))}
             </div>
           </>
