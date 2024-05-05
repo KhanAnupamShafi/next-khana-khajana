392d5b0d49a54d0d8b1fa1d9c30147256a8af347
KhanAnupamShafi <deep71.bd@gmail.com>
2024-05-05 10:07:41 +0600

    add priority images

--- components/home/landing/Hero.jsx
+++ components/home/landing/Hero.jsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 const Hero = () => {
   return (
-    <div className="py-4 bg-[url('/cover.png')] rounded-lg p-4 md:p-12 min-h-[450px] bg-cover grid place-items-center grid-cols-12">
-      <div className='col-span-12 md:col-span-6'>
+    <div className='relative py-4 bg-white rounded-lg p-4 md:p-12 min-h-[450px] grid place-items-center grid-cols-12'>
+      <div className='col-span-12 md:col-span-6 relative z-10'>
         <h1 className='font-bold text-3xl md:text-5xl text-white'>
           Choose from
           <br /> thousands of recipes
@@ -10,6 +12,17 @@ const Hero = () => {
           Appropriately integrate technically sound value with scalable
           infomediaries negotiate sustainable strategic theme areas
         </p>
+      </div>
+      <div className='absolute inset-0 z-0'>
+        <div className='bg-cover-image absolute inset-0' />
+        <Image
+          alt='Background'
+          src='/cover.png'
+          layout='fill'
+          objectFit='cover'
+          quality={100}
+          priority={true}
+        />
       </div>
     </div>
   );
